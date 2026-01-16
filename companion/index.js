21479a03a7b8eb014eed51137fce4e68b39c76f6
emrhky <emrhky.ek@gmail.com>
2026-01-16 21:27:07 +0300

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -1,39 +1,42 @@
 import { settingsStorage } from "settings";
 import * as messaging from "messaging";
 
-// Telefon tarafı açıldığında çalışır
 console.log("Companion (Telefon) tarafı çalışıyor.");
 
-// Mesajlaşma soketi açıldığında
 messaging.peerSocket.onopen = () => {
-  restoreHighScore();
+  restoreHighScores();
 };
 
-// Saatten mesaj geldiğinde (Yeni skor yapıldığında veya istek geldiğinde)
 messaging.peerSocket.onmessage = (evt) => {
   if (evt.data && evt.data.command === "SAVE_HIGHSCORE") {
-    // Gelen skoru telefon hafızasına kaydet
-    let currentSaved = parseInt(settingsStorage.getItem("highScore")) || 0;
+    // Gelen veri hangi moda ait?
+    const modeKey = evt.data.mode === "nowall" ? "highScoreNoWall" : "highScoreClassic";
+    const dateKey = evt.data.mode === "nowall" ? "dateNoWall" : "dateClassic";
     
+    let currentSaved = parseInt(settingsStorage.getItem(modeKey)) || 0;
+    
+    // Eğer yeni gelen skor telefondakinden büyükse kaydet
     if (evt.data.score > currentSaved) {
-      settingsStorage.setItem("highScore", evt.data.score.toString());
-      settingsStorage.setItem("highScoreDate", evt.data.date);
+      settingsStorage.setItem(modeKey, evt.data.score.toString());
+      settingsStorage.setItem(dateKey, evt.data.date);
     }
   } else if (evt.data && evt.data.command === "GET_HIGHSCORE") {
-    // Saat skor istediğinde gönder
-    restoreHighScore();
+    restoreHighScores();
   }
 };
 
-function restoreHighScore() {
-  let savedScore = parseInt(settingsStorage.getItem("highScore")) || 0;
-  let savedDate = settingsStorage.getItem("highScoreDate") || "";
-
+function restoreHighScores() {
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     messaging.peerSocket.send({
       command: "RESTORE_HIGHSCORE",
-      score: savedScore,
-      date: savedDate
+      classic: {
+        score: parseInt(settingsStorage.getItem("highScoreClassic")) || 0,
+        date: settingsStorage.getItem("dateClassic") || ""
+      },
+      nowall: {
+        score: parseInt(settingsStorage.getItem("highScoreNoWall")) || 0,
+        date: settingsStorage.getItem("dateNoWall") || ""
+      }
     });
   }
 }
